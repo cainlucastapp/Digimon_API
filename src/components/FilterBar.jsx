59316d74b9d0e01939a4607070974694be9e4084
cainlucastapp <cainlucastapp@agentformula.com>
2026-06-03 10:33:50 -0700

    Wired pages and components while testing and building API

--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -1,1 +1,30 @@
-#src/components/FilterBar.jsx
+// src/components/FilterBar.jsx
+
+import { useCallback } from "react"
+import useFetch from "../hooks/useFetch"
+import { getAttributeList, getLevelList } from "../services/digimonApi"
+
+function FilterBar() {
+  const fetchAttributes = useCallback(() => getAttributeList(), [])
+  const fetchLevels = useCallback(() => getLevelList(), [])
+
+  const { data: attributes, loading: attrLoading } = useFetch(fetchAttributes, [])
+  const { data: levels, loading: levelLoading } = useFetch(fetchLevels, [])
+
+  if (attrLoading || levelLoading) return <div>Loading filters...</div>
+
+  return (
+    <div>
+      <div>
+        <strong>Attributes:</strong>
+        {attributes?.map((a) => <div key={a.id}>{a.name}</div>)}
+      </div>
+      <div>
+        <strong>Levels:</strong>
+        {levels?.map((l) => <div key={l.id}>{l.name}</div>)}
+      </div>
+    </div>
+  )
+}
+
+export default FilterBar
