64a93c6730c87064d4bf8166ac6e90e0a76f7be8
cainlucastapp <cainlucastapp@agentformula.com>
2026-06-04 09:58:28 -0700

    navbar styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,35 @@
 // src/components/Navbar.jsx
 
+import { useState } from "react"
 import { Link } from "react-router-dom"
+import digimonLogo from "../assets/digimon_logo.png"
+import "../styles/Navbar.css"
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  const closeMenu = () => setMenuOpen(false)
+
   return (
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/digimon">List</Link>
+    <nav className="navbar">
+      <Link to="/" className="navbar__logo" onClick={closeMenu}>
+        <img src={digimonLogo} alt="Digimon Logo" />
+      </Link>
+
+      <button
+        className={`navbar__hamburger ${menuOpen ? "open" : ""}`}
+        onClick={() => setMenuOpen(!menuOpen)}
+        aria-label="Toggle menu"
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+
+      <ul className={`navbar__links ${menuOpen ? "open" : ""}`}>
+        <li><Link to="/" onClick={closeMenu}>Home</Link></li>
+        <li><Link to="/digimon" onClick={closeMenu}>Digimons</Link></li>
+      </ul>
     </nav>
   )
 }
