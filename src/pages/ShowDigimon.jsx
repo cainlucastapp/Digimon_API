d45cd5df768738eda6acf9d0327bb2e13eaf64a9
cainlucastapp <cainlucastapp@agentformula.com>
2026-06-10 11:47:16 -0700

    refactored ShowDigimon into seperate componenets

--- src/pages/ShowDigimon.jsx
+++ src/pages/ShowDigimon.jsx
@@ -4,6 +4,9 @@ import { useParams } from "react-router-dom"
 import { useCallback } from "react"
 import useFetch from "../hooks/useFetch"
 import { getDigimon } from "../services/digimonApi"
+import DigimonCard from "../components/DigimonCard"
+import DigimonDetails from "../components/DigimonDetails"
+import DigimonSkills from "../components/DigimonSkills"
 import EvolutionChain from "../components/EvolutionChain"
 import "../styles/ShowDigimon.css"
 
@@ -18,54 +21,14 @@ function ShowDigimon() {
   return (
     <div className="show">
       <div className="show-main">
-
         <div className="show-left">
-          <div className="show-left-header">
-            <span className="show-name">{data.name}</span>
-          </div>
-          <img
-            src={data.images?.[0]?.href}
-            alt={data.name}
-            className="show-image"
-          />
-          <div className="show-left-footer">
-            <span className="show-id">#{data.id}</span>
-            {data.xAntibody && <span className="show-xantibody">X</span>}
-          </div>
+          <DigimonCard digimon={data} onClick={() => {}} />
         </div>
-
         <div className="show-right">
-          <div className="show-badges">
-            {data.levels?.map((l) => (
-              <span key={l.id} className="show-badge show-badge-level">{l.level}</span>
-            ))}
-            {data.attributes?.map((a) => (
-              <span key={a.id} className="show-badge show-badge-attribute">{a.attribute}</span>
-            ))}
-            {data.types?.map((t) => (
-              <span key={t.id} className="show-badge show-badge-type">{t.type}</span>
-            ))}
-            {data.fields?.map((f) => (
-              <img key={f.id} src={f.image} alt={f.field} className="show-field-icon" title={f.field} />
-            ))}
-          </div>
-
-          <div className="show-description">
-            {data.descriptions?.find(d => d.language === "en_us")?.description || "No description available."}
-          </div>
+          <DigimonDetails data={data} />
         </div>
-
       </div>
-
-      <div className="show-skills">
-        <h3 className="show-section-title">Skills</h3>
-        {data.skills?.map((s) => (
-          <div key={s.id} className="show-skill">
-            <span className="show-skill-name">{s.skill}</span>
-            <p className="show-skill-desc">{s.description}</p>
-          </div>
-        ))}
-      </div>
+      <DigimonSkills skills={data.skills} />
       <EvolutionChain prior={data.priorEvolutions} next={data.nextEvolutions} />
     </div>
   )
