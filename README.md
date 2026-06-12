016b2dbdb4b1320230907cf8bab32075c740f4b3
cainlucastapp <cainlucastapp@agentformula.com>
2026-06-12 14:21:20 -0700

    added api credit to the footer

--- README.md
+++ README.md
@@ -16,5 +16,3 @@ Setting for a non responsive search
 Dispite the API working in earlier forms I had to keep going back to it as issues arose. 
 learned about skeleton cards, even though they didnt fit in with the format.
 
-
-Remember to give credit to the API creator in the final readme.
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,6 +5,16 @@ import "../styles/Footer.css"
 function Footer() {
   return (
     <footer className="footer">
+      <p className="footer-powered">
+        Powered by{" "}
+        <a href="https://www.linkedin.com/in/vinícius-brito-costa/" target="_blank" rel="noreferrer">
+          Vinicius Brito Costa
+        </a>
+        's{" "}
+        <a href="https://digi-api.com/" target="_blank" rel="noreferrer">
+          digi-api.com
+        </a>
+      </p>
       <p className="footer-disclaimer">
         Digimon and other media relating to the franchise are registered trademarks of Bandai.
       </p>
