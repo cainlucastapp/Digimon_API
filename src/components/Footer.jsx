b82ecd2204d4655cd5d64a9b58c81adef2a510c4
cainlucastapp <cainlucastapp@agentformula.com>
2026-06-12 12:11:34 -0700

    Footer Component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+// src/components/Footer.jsx
+
+import "../styles/Footer.css"
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer-disclaimer">
+        Digimon and other media relating to the franchise are registered trademarks of Bandai.
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
