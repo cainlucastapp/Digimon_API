3d35ae11aa7ab5c895ffa7a2187481dbc1f6db69
cainlucastapp <cainlucastapp@agentformula.com>
2026-06-12 15:13:13 -0700

    Snap to top

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import Footer from "./components/Footer"
 import Navbar from "./components/Navbar"
+import ScrollToTop from "./components/ScrollToTop"
 import Hero from "./pages/Hero"
 import List from "./pages/List"
 import NotFound from "./pages/NotFound"
@@ -11,6 +12,7 @@ import ShowDigimon from "./pages/ShowDigimon"
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Navbar />
       <Routes>
         <Route path="/" element={<Hero />} />
