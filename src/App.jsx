b36e9d8c27d0f2486fa23f1910efe924c348e67e
cainlucastapp <cainlucastapp@agentformula.com>
2026-06-16 11:09:07 -0700

    Added Battle Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,38 @@
 // src/App.jsx
 
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Navigate, useLocation } from "react-router-dom"
 import Footer from "./components/Footer"
 import Navbar from "./components/Navbar"
 import ScrollToTop from "./components/ScrollToTop"
+import Battle from "./pages/Battle"
 import Hero from "./pages/Hero"
 import List from "./pages/List"
 import NotFound from "./pages/NotFound"
 import ShowDigimon from "./pages/ShowDigimon"
 
-function App() {
+function AppRoutes() {
+  const location = useLocation()
+
   return (
-    <BrowserRouter>
+    <>
       <ScrollToTop />
       <Navbar />
       <Routes>
         <Route path="/" element={<Hero />} />
         <Route path="/digimon" element={<List />} />
         <Route path="/digimon/:id" element={<ShowDigimon />} />
+        <Route path="/battle" element={<Battle key={location.key} />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
+    </>
+  )
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AppRoutes />
     </BrowserRouter>
   )
 }
