551f5b4c57ba2eee67bc6319626f290727a48621
cainlucastapp <cainlucastapp@agentformula.com>
2026-06-19 09:25:15 -0700

    Added scan line to card as image loader

--- src/components/DigimonCard.jsx
+++ src/components/DigimonCard.jsx
@@ -1,10 +1,12 @@
 // src/components/DigimonCard.jsx
 
 import { useNavigate } from "react-router-dom"
+import { useState } from "react"
 import "../styles/DigimonCard.css"
 
 function DigimonCard({ digimon }) {
   const navigate = useNavigate()
+  const [imageLoaded, setImageLoaded] = useState(false)
 
   const handleClick = () => navigate(`/digimon/${digimon.id}`)
 
@@ -14,12 +16,19 @@ function DigimonCard({ digimon }) {
         <span className="card-name">{digimon.name}</span>
       </div>
       <div className="card-image-container">
+        {!imageLoaded && (
+            <div className="card-image-scan">
+                <div className="card-image-scan-line" />
+            </div>
+        )}
         <img
-          src={digimon.images?.[0]?.href || digimon.image}
-          alt={digimon.name}
-          className="card-image"
+            src={digimon.images?.[0]?.href || digimon.image}
+            alt={digimon.name}
+            className="card-image"
+            style={{ display: imageLoaded ? "block" : "none" }}
+            onLoad={() => setImageLoaded(true)}
         />
-      </div>
+    </div>
       <div className="card-footer">
         <span className="card-id">#{digimon.id}</span>
       </div>
